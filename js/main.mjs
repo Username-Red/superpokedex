b90b7ab41a155ae69e3f69f94d9d4221a398e664
Username-Red <ashkestrel@gmail.com>
2024-12-07 21:29:55 +0200

    moving everything

---
+++ js/main.mjs
@@ -0,0 +1,9 @@
+import { captureBtn, encounterBtn, getDesc, giveSprite, initializeLists, loadHeaderFooter} from "./utils.mjs";
+
+loadHeaderFooter()
+initializeLists();
+// getPkmnInfo();
+giveSprite("lugia");
+getDesc("lugia");
+encounterBtn();
+captureBtn();
